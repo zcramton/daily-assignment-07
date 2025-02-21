69727a96338953964935f455a28f4876162bfa45
ZCramton <zachary@cramton.com>
2025-02-20 21:59:54 -0700

    Header commit

---
+++ R/day-07.R
@@ -0,0 +1,25 @@
+# Zachary Cramton
+# 20 Feburary 2025
+# ESS 330 - Daily Assignment 07
+# This is the R script for assignment 07 where the COVID-19 data from 
+# assignment 06 will be visualized.
+
+## COVID-19 Data
+
+### Data
+
+# We are going to practice some data wrangling skills using a real-world 
+# dataset about COVID cases curated and maintained by the New York Times. The
+# data are archived on a GitHub repo [here](https://github.com/nytimes/covid-19-data). 
+```
+{r, eval = FALSE}
+https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-recent.csv
+```
+
+# Read-in and store NY-Times data
+```{r, message=FALSE, warning=FALSE}
+library(tidyverse)
+url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-recent.csv'
+covid = read_csv(url)
+head(covid, 5)
+```
