82b9f0ba98a02415eef4a7eb982aa23511c94b8e
ZCramton <zachary@cramton.com>
2025-02-20 22:18:13 -0700

    Recommit with content form day 06.

--- R/day-07.R
+++ R/day-07.R
@@ -9,17 +9,24 @@
 ### Data
 
 # We are going to practice some data wrangling skills using a real-world 
-# dataset about COVID cases curated and maintained by the New York Times. The
+# data set about COVID cases curated and maintained by the New York Times. The
 # data are archived on a GitHub repo [here](https://github.com/nytimes/covid-19-data). 
-```
-{r, eval = FALSE}
-https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-recent.csv
-
 
 # Read-in and store NY-Times data
-```{r, message=FALSE, warning=FALSE}
 library(tidyverse)
+library(dplyr)
 url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties-recent.csv'
 covid = read_csv(url)
-head(covid, 5)
-```
+
+# Find the most recent date
+max_date <- max(covid$date)
+most_recent_data <- filter(covid, date == max_date)
+
+# Get the 5 counties with the most cases
+top_6_states_cases <- most_recent_data %>%
+  arrange(desc(cases)) %>% 
+  slice(1:6)
+
+top_6_state_cases
+
+
